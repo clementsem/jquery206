3eac30f9c53c7cff18145883b0763b114446be77
clementsem <clementctes@gmail.com>
2019-02-04 09:49:56 +0100

    exo6

---
+++ assets/js/script.js
@@ -0,0 +1,21 @@
+$(document).ready(function(){
+
+  $("#green").mouseenter(function(){
+     $(text).css("color", "green");
+   });
+   $("#green").mouseleave(function(){
+     $(text).css("color", "black");
+   });
+   $("#red").mouseenter(function(){
+      $(text).css("color", "red");
+    });
+    $("#red").mouseleave(function(){
+      $(text).css("color", "black");
+    });
+    $("#blue").mouseenter(function(){
+       $(text).css("color", "blue");
+     });
+     $("#blue").mouseleave(function(){
+       $(text).css("color", "black");
+     });
+})
